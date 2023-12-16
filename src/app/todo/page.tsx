7d10d55a6fed7e2592657b845e425fbdd6245cb7
Pyry Kaleva <pyrykaleva@fastmail.com>
2023-12-16 16:00:34 +0200

    Added functionality and prettied up todo list

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 interface Item {
     id: number;
     value: String;
+    done: boolean;
 }
 
 const Todo = () => {
@@ -21,6 +22,7 @@ const Todo = () => {
         const new_value: Item = {
             id: Math.floor(Math.random() * 10000),
             value: value,
+            done: false,
         };
 
         let tmp = [...list];
@@ -37,19 +39,60 @@ const Todo = () => {
             tmp.splice(to_del, 1);
         }
         setList(tmp);
-        console.log("This'll be removed: ", to_del);
+    };
+
+    const moveItem = (id: number) => {
+        if (list.length < 1) return;
+        let tmp = [...list];
+        const to_move = list.findIndex(e => e.id === id);
+        if (to_move === 0) return;
+        [tmp[to_move], tmp[to_move - 1]] = [tmp[to_move - 1], tmp[to_move]];
+        setList(tmp);
+    };
+
+    const updateShow = (id: number) => {
+        let tmp = [...list];
+        const to_edit = list.findIndex(e => e.id === id);
+        tmp[to_edit].done = !tmp[to_edit].done;
+        setList(tmp);
     };
 
     return (
-        <div>
-            <input type="text" onChange={handleChange} value={value}></input>
-            <button onClick={() => addToDo()}>Add To Do</button>
+        <div className="mx-auto rounded-xl rounded-b-xl bg-slate-200 bg-opacity-40 p-5 shadow-xl lg:w-1/2 lg:overflow-auto">
+            <div>
+                <input
+                    className="m-2 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-lg text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                    type="text"
+                    onChange={handleChange}
+                    value={value}></input>
+                <button
+                    className="m-2 rounded bg-blue-500 px-2 py-1 font-bold text-white hover:bg-blue-700"
+                    onClick={() => addToDo()}>
+                    Add To Do
+                </button>
+            </div>
             <ul>
                 {list ? (
                     list.map(e => (
-                        <li key={e.id}>
-                            <button onClick={() => removeItem(e.id)}>X</button>
-                            &nbsp;{e.value}
+                        <li key={e.id} className={`my-2 select-none`}>
+                            <button
+                                className="py-1/2 mr-2 rounded bg-red-500 px-2 text-lg font-bold text-white hover:bg-red-700"
+                                onClick={() => removeItem(e.id)}>
+                                X
+                            </button>
+                            <button
+                                className="py-1/2 rounded bg-blue-500 px-2 text-lg font-bold text-white hover:bg-blue-700"
+                                onClick={() => moveItem(e.id)}>
+                                ^
+                            </button>
+                            &nbsp;
+                            <span
+                                className={`text-xl text-gray-900 ${
+                                    e.done ? 'line-through' : ''
+                                }`}
+                                onClick={() => updateShow(e.id)}>
+                                {e.value}
+                            </span>
                         </li>
                     ))
                 ) : (
@@ -62,7 +105,7 @@ const Todo = () => {
 
 export default function Page() {
     return (
-        <div className="h-screen bg-[url('./img.jpg')] bg-cover p-6 lg:grid lg:grid-cols-3 lg:grid-rows-1 lg:gap-10">
+        <div className="h-screen bg-[url('./img.jpg')] bg-cover p-6">
             <Todo />
         </div>
     );
